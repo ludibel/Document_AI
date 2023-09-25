69488ce3c9f6dea436aecd6aead9adcfa6adbf05
ludibel <ludibelle@outlook.com>
2023-09-25 10:50:16 +0200

    ajout des fichiers pour la configuration du theme personnalisé

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ThemeProvider as MuiThemeProvider } from '@mui/material/styles'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+import theme from '@/styles/themePerso'
+
+const App = ({ Component, pageProps }: AppProps) => (
+  <MuiThemeProvider theme={theme}>
+    <Component {...pageProps} />
+  </MuiThemeProvider>
+)
+
+export default App
